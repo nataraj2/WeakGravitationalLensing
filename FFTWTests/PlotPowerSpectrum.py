ee4dfea93cd9b98be9e9c2922a0c8f54015a2a5c
Mahesh Natarajan <natarajan@Maheshs-MacBook-Pro.local>
2024-12-19 02:19:27 -0800

    Updating Python script

--- FFTWTests/PlotPowerSpectrum.py
+++ FFTWTests/PlotPowerSpectrum.py
@@ -1,5 +1,7 @@
 from numpy import *
 import matplotlib.pyplot as plt
+import os
+
 
 data = loadtxt("k_vs_Pk.txt");
 k = data[:,0]
@@ -13,5 +15,13 @@ plt.loglog(k, Pk_exact, 'ok', label='Specified')
 plt.xlabel(r'$k$',fontsize=15)
 plt.ylabel(r'$P(k)$',fontsize=15)
 plt.legend()
+# Define the directory name
+directory = "Images"
+
+# Check if the directory exists
+if not os.path.exists(directory):
+    # Create the directory
+    os.makedirs(directory)
+
 plt.savefig('./Images/kvsPk.png')
 plt.show()
